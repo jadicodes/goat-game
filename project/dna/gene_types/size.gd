d246fdd53fa55a1c7685c1dc446caa31b4ac8fd2
Jeffrey Harmon <contact@jeffreyharmon.dev>
2025-06-21 20:43:06 -0400

    Add Goat size handling
    Signed-off-by: Jeffrey Harmon <contact@jeffreyharmon.dev>

--- project/dna/gene_types/size.gd
+++ project/dna/gene_types/size.gd
@@ -12,15 +12,30 @@ enum Size {
 	LARGE,
 }
 
+func get_default_value() -> Vector2i:
+	return Vector2i(Allele.SMALL, Allele.LARGE)
+
 
 func get_type_enum() -> DNA.GeneType:
 	return DNA.GeneType.SIZE
 
 
-func get_size() -> int:
+func get_size() -> Size:
 	if value == Vector2i(Allele.SMALL, Allele.SMALL):
 		return Size.SMALL
 	elif value == Vector2i(Allele.LARGE, Allele.LARGE):
 		return Size.LARGE
 
 	return Size.MEDIUM
+
+
+func get_scale() -> float:
+	match get_size():
+		Size.SMALL:
+			return 0.5
+		Size.MEDIUM:
+			return 1.0
+		Size.LARGE:
+			return 1.5
+		_:
+			return 1.0  # Should never happen, but a default is needed
--- project/goat/goat.gd
+++ project/goat/goat.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 @export var dna: DNA
 
 @onready var sprite: Sprite2D = $Sprite2D
+@onready var collision_shape: CollisionShape2D = $CollisionShape2D
 
 
 func _ready() -> void:
@@ -11,7 +12,12 @@ func _ready() -> void:
 	dna.get_gene(DNA.GeneType.COAT).set_value(CoatGene.Allele.BROWN if randi() % 2 == 0 else CoatGene.Allele.BLACK)
 
 	set_color(dna.get_gene(DNA.GeneType.COAT).get_color())
+	set_size(dna.get_gene(DNA.GeneType.SIZE).get_scale())
 
 
 func set_color(color: Color) -> void:
 	sprite.modulate = color
+
+
+func set_size(new_scale: float) -> void:
+	sprite.scale = Vector2(new_scale, new_scale)
