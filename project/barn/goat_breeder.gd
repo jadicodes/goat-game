19840ae82ef6dc2df30bb896ada0f68071f5a9a3
Jeffrey Harmon <contact@jeffreyharmon.dev>
2025-06-23 21:09:51 -0400

    Set goats to spawn in center of screen

--- project/barn/goat_breeder.gd
+++ project/barn/goat_breeder.gd
@@ -4,5 +4,6 @@ extends Node
 
 func _breed(goat1, goat2):
 	var new_goat = Goat.create(DNA.combine(goat1.dna, goat2.dna))
+	new_goat.global_position = get_viewport().size / 2
 	get_tree().get_root().add_child(new_goat)
 	print("Making a sweet angel goat")
