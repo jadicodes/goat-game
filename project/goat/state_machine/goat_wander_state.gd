0d5aeeed9215d623731c233b17e4c3feb6b85e17
Jeffrey Harmon <contact@jeffreyharmon.dev>
2025-06-28 16:43:38 -0400

    Add spread to goat wandering

--- project/goat/state_machine/goat_wander_state.gd
+++ project/goat/state_machine/goat_wander_state.gd
@@ -14,11 +14,38 @@ func _ready() -> void:
 
 
 func enter(_previous_state_path: String, _data := {}) -> void:
-	goat.velocity = Vector2.from_angle(randf_range(0, TAU)) * goat.speed
+	var overlap: Array[Node2D] = []
+
+	for area in goat.hit_box.get_overlapping_areas():
+		overlap.append(area)
+
+	for body in goat.hit_box.get_overlapping_bodies():
+		if body == goat:
+			continue
+		overlap.append(body)
+
+	if overlap.is_empty():
+		goat.velocity = Vector2.from_angle(randf_range(0, TAU)) * goat.speed
+	else:
+		_move_away(overlap)
 	# goat.animation_player.play("run")
 
 	timer.wait_time = randf_range(1, 3)
 	timer.start()
+
+
+func _move_away(nodes: Array[Node2D]) -> void:
+	var average_position := Vector2.ZERO
+
+	for node in nodes:
+		average_position += node.global_position
+
+	average_position /= nodes.size()
+
+	goat.velocity = (goat.global_position - average_position).normalized() * goat.speed
+
+	# Add some randomness to the direction
+	goat.velocity = goat.velocity.rotated(randf_range(-PI / 4, PI / 4))
 
 
 func _on_timer_timeout() -> void:
