492473c1e8b72b6167fdb59473116c2a606904af
Jeffrey Harmon <contact@jeffreyharmon.dev>
2025-06-28 18:01:11 -0400

    Fix single goat invisibility

--- project/goat/state_machine/goat_hidden_state.gd
+++ project/goat/state_machine/goat_hidden_state.gd
@@ -3,19 +3,23 @@ extends GoatState
 
 
 var _start_color: Color
-
+var _tween: Tween
 
 func enter(_previous_state_path: String, _data := {}) -> void:
 	_start_color = Color(goat.modulate)
-	print(_start_color)
 	goat.velocity = Vector2.ZERO
-	var tween: Tween = get_tree().create_tween()
-	tween.tween_property(goat, "modulate", Color.hex(0x1d1c2a00), 1)
-	tween.tween_callback(func() -> void:
-		goat.visible = false)
+	_tween = get_tree().create_tween()
+	_tween.tween_property(goat, "modulate", Color.hex(0x1d1c2a00), 1)
+	_tween.tween_callback(func() -> void:
+		goat.visible = false
+	)
 
 
 func exit() -> void:
-	goat.modulate = _start_color
 	goat.visible = true
-	print(_start_color)
+
+	if not _tween:
+		return
+
+	_tween.kill()
+	goat.modulate = _start_color
