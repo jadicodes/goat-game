0ad55e657bfa1537ad49fb7b95851e98bebc57fc
Jadi Miller <jadilmiller@gmail.com>
2025-06-28 14:52:53 -0400

    Spawn in shoppers ever 35 seconds

---
+++ project/farm/farm.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+@export var shopper_scene: PackedScene
+
+
+func _create_shopper():
+	var shopper = shopper_scene.instantiate()
+	var shopper_spawn_location = %ShopperSpawnLocation
+	shopper_spawn_location.progress_ratio = randf()
+
+	shopper.global_position = shopper_spawn_location.global_position
+	get_tree().get_root().add_child(shopper)
+	print("shopper created")
+
+
+func _on_timer_timeout() -> void:
+	_create_shopper()
--- project/shopper/state_machine/shopper_enter_state.gd
+++ project/shopper/state_machine/shopper_enter_state.gd
@@ -2,10 +2,16 @@ class_name ShopperEnterState
 extends ShopperState
 
 
+var _order_point: Vector2
+
 func enter(_previous_state_path: String, _data := {}) -> void:
 	shopper.velocity = (
 		((get_viewport().size / 2.0) - shopper.global_position).normalized() * shopper.speed
 	)
+	print("shopper position: ", shopper.global_position)
+	print(shopper.velocity)
+	print(get_viewport().size)
+	print("viewport: " + str(get_viewport().size / 2.0))
 
 	await get_tree().create_timer(5).timeout
 
