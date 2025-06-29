810dccb086216772c434e503d8e84da46d8249ec
Jeffrey Harmon <contact@jeffreyharmon.dev>
2025-06-28 23:07:12 -0400

    Update shopper state logic
    Co-authored-by: Hunter Johns <hunter@whichfishstudios.com>

--- project/shopper/state_machine/shopper_arrive_at_fence_state.gd
+++ project/shopper/state_machine/shopper_arrive_at_fence_state.gd
@@ -1,14 +1,32 @@
 class_name ShopperArriveAtFenceState
 extends ShopperState
 
+var timer: Timer
+
+
+func _ready() -> void:
+	super._ready()
+	timer = Timer.new()
+	timer.autostart = false
+	timer.one_shot = true
+	timer.timeout.connect(_on_timer_timeout)
+	add_child(timer)
+
 
 func enter(_previous_state_path: String, _data := {}) -> void:
 	shopper.velocity = Vector2.ZERO
 	# shopper.animation_player.play("idle")
+
+	timer.start(5)
 
 
 func interact(caller: Node) -> void:
 	if not caller is Farmer:
 		return
 
+	timer.stop()
 	finished.emit(GIVE_ORDER)
+
+
+func _on_timer_timeout() -> void:
+	finished.emit(ENTER)
--- project/shopper/state_machine/shopper_leave_state.gd
+++ project/shopper/state_machine/shopper_leave_state.gd
@@ -7,6 +7,10 @@ func enter(_previous_state_path: String, _data := {}) -> void:
 		(shopper.global_position - (get_viewport().size / 2.0)).normalized() * shopper.speed
 	)
 
+	# Disable collision so shopper can leave the screen
+	shopper.collision_layer = 0
+	shopper.collision_mask = 0
+
 	await get_tree().create_timer(5).timeout
 
 	shopper.queue_free()
