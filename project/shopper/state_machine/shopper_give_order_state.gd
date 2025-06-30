0159d0349e7a5c4eb78247343dc535e11a9bd56e
Hunter Johns <Hunter@whichfishstudios.com>
2025-06-29 21:14:11 -0600

    Add both size and color requirements

--- project/shopper/state_machine/shopper_give_order_state.gd
+++ project/shopper/state_machine/shopper_give_order_state.gd
@@ -11,10 +11,14 @@ func enter(_previous_state_path: String, _data := {}) -> void:
 	var order_data := _generate_order()
 
 	await get_tree().create_timer(1.0).timeout
-	
+	if not order_data.has("secondary_type"):
+		var gene_type_name: String = DNA.GeneType.keys()[order_data.gene_type]
+		%SpeechBubble.display_text(tr("%s_%d" % [gene_type_name, order_data.phenotype]))
 
-	var gene_type_name: String = DNA.GeneType.keys()[order_data.gene_type]
-	%SpeechBubble.display_text(tr("%s_%d" % [gene_type_name, order_data.phenotype]))
+	if order_data.has("secondary_type"):
+		var gene_type_name: String = DNA.GeneType.keys()[order_data.gene_type]
+		var secondary_gene_type_name: String = DNA.GeneType.keys()[order_data.secondary_type]
+		%SpeechBubble.display_text(tr("%s_%d" % [secondary_gene_type_name, order_data.secondary_phenotype]) +"\n"+ tr("%s_%d" % [gene_type_name, order_data.phenotype]))
 
 	finished.emit(WAIT_FOR_ORDER, {"order_data": order_data})
 
@@ -30,11 +34,17 @@ func _generate_order() -> Dictionary:
 
 	var order_data: Dictionary = {"gene_type": gene_type, "phenotype": phenotype}
 
-	if days >2 and days <7:
+	if days >3:
 		gene_type = [DNA.GeneType.SIZE].pick_random()
 
 		phenotype = DNA.get_gene_class(gene_type).get_random_phenotype()
-
-		order_data = {"gene_type": gene_type, "phenotype": phenotype}
+		if days <8:
+			order_data = {"gene_type": gene_type, "phenotype": phenotype}
+			print(order_data)
+		else:
+			order_data["secondary_type"] = gene_type
+			order_data["secondary_phenotype"] = phenotype
+			print(order_data)
+		
 
 	return order_data
